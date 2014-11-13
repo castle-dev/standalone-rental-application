4b770ccd89583d07c5e30408bae0d4ef4e8a545b
Scott Lowe <me@scottdlowe.com>
2014-11-13 13:06:21 -0500

    Generated angular app scaffold with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name propertyManagementAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the propertyManagementAppApp
+ */
+angular.module('propertyManagementAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
