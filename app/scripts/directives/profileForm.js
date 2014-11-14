3627aa78f81db53b88897e1e7e169f3ff973bb2a
Scott Lowe <me@scottdlowe.com>
2014-11-14 17:47:40 -0500

    Creating new directives for signup and profile

---
+++ app/scripts/directives/profileForm.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name propertyManagementApp.directive:profileForm
+ * @description
+ * # profileForm
+ * Directive containing template and logic
+ * for updating a user's profile
+ */
+angular.module('propertyManagementApp')
+  .directive('profileForm', function () {
+    return {
+      template: 'TODO: Profile form template'
+    };
+  });
+
---
+++ app/scripts/directives/signupForm.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name propertyManagementApp.directive:signupForm
+ * @description
+ * # signupForm
+ * Directive containing template and logic
+ * for creating a new acount
+ */
+angular.module('propertyManagementApp')
+  .directive('signupForm', function () {
+    return {
+      template: 'TODO: Signup form template'
+    };
+  });
