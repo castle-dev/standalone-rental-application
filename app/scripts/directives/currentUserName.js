73f1e927561b809c17dfa71eb264735a14a71580
Scott Lowe <me@scottdlowe.com>
2014-11-14 19:22:18 -0500

    Creating a directive for display the user's name

---
+++ app/scripts/directives/currentUserName.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name propertyManagementApp.directive:currentUserName
+ * @description
+ * # currentUserName
+ * Directive that display's the user's first name 
+ */
+angular.module('propertyManagementApp')
+  .directive('currentUserName', function () {
+    return {
+      template: 'TODO: Implement currentUserName directive'
+    };
+  });
