6431f5f074d7e07836d0784646a81d64c82df4a6
Scott Lowe <me@scottdlowe.com>
2014-12-02 22:29:39 -0500

    Storing data from application form to firebase

--- app/scripts/directives/rentalApplicationForm.js
+++ app/scripts/directives/rentalApplicationForm.js
@@ -9,14 +9,21 @@
  * for signing in
  */
 angular.module('propertyManagementApp')
-  .directive('rentalApplicationForm', function () {
+  .directive('rentalApplicationForm', function ($firebase, FIREBASE_URL, $window, $anchorScroll) {
     return {
       restrict: 'E',
       templateUrl: 'views/partials/rentalApplicationForm.html',
       link: function (scope) {
+        var ref = new $window.Firebase(FIREBASE_URL);
+        var applicants = $firebase(ref.child('applicant')).$asArray();
+
         scope.submit = function () {
-          console.log('to space!');
-        }
+          applicants.$add(scope.applicant)
+            .then(function () {
+              scope.submitted = true;
+              $anchorScroll();
+            });
+        };
       }
     };
   });
