0da07de80d3f8022474fe244033712d54bbd4d68
Scott Lowe <scott@entercastle.com>
2015-01-29 16:48:39 -0500

    Adding S3 credentials to config unit tests

--- test/unit/config.js
+++ test/unit/config.js
@@ -1,16 +1,24 @@
 'use strict';
 
-describe('Module: config', function () {
+describe('Environment variables', function () {
 
   // load the controller's module
   beforeEach(module('config'));
 
-  var env, firebaseUrl, stripePublishableKey;
+  var env,
+      firebaseUrl,
+      stripePublishableKey,
+      amazonS3PublicAccessKey,
+      amazonS3PublicSecretKey,
+      amazonS3PublicBucket;
 
   beforeEach(inject(function($injector) {
     env = $injector.get('ENV');
     firebaseUrl = $injector.get('FIREBASE_URL');
     stripePublishableKey = $injector.get('STRIPE_PUBLISHABLE_KEY');
+    amazonS3PublicAccessKey = $injector.get('AMAZON_S3_PUBLIC_ACCESS_KEY');
+    amazonS3PublicSecretKey = $injector.get('AMAZON_S3_PUBLIC_SECRET_KEY');
+    amazonS3PublicBucket = $injector.get('AMAZON_S3_PUBLIC_BUCKET');
   }));
 
 
@@ -26,5 +34,14 @@ describe('Module: config', function () {
   it('should have a stripe API key defined', function () {
     expect(stripePublishableKey).toBeDefined();
     expect(stripePublishableKey).not.toEqual('');
+  });
+
+  it('should have public Amazon S3 credentials', function () {
+    expect(amazonS3PublicAccessKey).toBeDefined();
+    expect(amazonS3PublicAccessKey).not.toEqual('');
+    expect(amazonS3PublicSecretKey).toBeDefined();
+    expect(amazonS3PublicSecretKey).not.toEqual('');
+    expect(amazonS3PublicBucket).toBeDefined();
+    expect(amazonS3PublicBucket).not.toEqual('');
   });
 });
