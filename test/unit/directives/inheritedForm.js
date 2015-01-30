a3483bded248cf7e61f5341f728f647e0fc9b911
Scott Lowe <scott@entercastle.com>
2015-01-30 12:35:45 -0500

    Adding more unit tests to inherited tenant form

--- test/unit/directives/inheritedForm.js
+++ test/unit/directives/inheritedForm.js
@@ -2,7 +2,7 @@
 
 describe('Directive: inheritedForm', function() {
 
-  var element, scope, inputs, routeParams;
+  var element, scope, routeParams, form;
 
   // load the directive's module and mock dependencies
   beforeEach(module('propertyManagementApp', function ($provide) {
@@ -24,9 +24,9 @@ describe('Directive: inheritedForm', function() {
     element = angular.element(
       '<inherited-form></inherited-form>'
     );
-    $compile(element)(scope);
+    element = $compile(element)(scope);
     scope.$apply();
-    inputs = element.find('input');
+    form = scope.inherited;
   }));
 
   it('should respect logic', function () {
@@ -35,6 +35,40 @@ describe('Directive: inheritedForm', function() {
 
   it('should read the address from the url', function () {
     expect(scope.address).toEqual(routeParams.address);
+  });
+
+  it('should have a form', function () {
+    expect(form).toBeDefined();
+  });
+
+  var enterContactInfo = function (onLease) {
+    form.firstname.$setViewValue('Optimus');
+    form.lastname.$setViewValue('Prime');
+    form.email.$setViewValue('optimus.prime@gmail.com');
+    form.phone.$setViewValue('1011011100');
+    form.otheroccupants.$setViewValue('');
+    form.pets.$setViewValue('');
+    form.onlease.$setViewValue(onLease);
+  };
+
+  it('should require tenant contact info', function () {
+    expect(form.$valid).toBeFalsy();
+    enterContactInfo(false);
+    expect(form.$valid).toBeTruthy();
+  });
+
+  it('should require tenants with leases to upload them', function () {
+    expect(form.$valid).toBeFalsy();
+    enterContactInfo(true);
+    expect(form.$valid).toBeFalsy();
+  });
+
+  // Causes a page refresh which karma isn't cool with, but this is close
+  xit('should submit when the form is valid', function () {
+    spyOn(scope, 'submit').and.callThrough();
+    enterContactInfo(false);
+    element.find('form').submit();
+    expect(scope.submit).toHaveBeenCalled();
   });
 
 });
