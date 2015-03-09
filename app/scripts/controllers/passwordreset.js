68b5d346d1c5cfaf99c981ac90a9dde3aaa212ed
Scott Lowe <scott@entercastle.com>
2015-03-09 00:25:14 -0400

    Generating Password reset controller

---
+++ app/scripts/controllers/passwordreset.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name propertyManagementApp.controller:PasswordresetCtrl
+ * @description
+ * # PasswordresetCtrl
+ * Controller of the propertyManagementApp
+ */
+angular.module('propertyManagementApp')
+  .controller('PasswordResetCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
