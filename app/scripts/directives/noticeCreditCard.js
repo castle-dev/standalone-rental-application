9922a4741d56c2c51bf9559bf3f01ea12f5a86b4
Scott Lowe <scott@entercastle.com>
2015-03-09 04:08:30 -0400

    Updating conditional logic for bank and cc notices

--- app/scripts/directives/noticeCreditCard.js
+++ app/scripts/directives/noticeCreditCard.js
@@ -15,9 +15,9 @@ angular.module('propertyManagementApp')
       templateUrl: 'views/partials/notice.html',
       scope: {},
       link: function (scope, elem) {
-        scope.text = 'It appears we don\'t have a valid credit card on file for your account';
-        scope.nextStep = 'Enter your credit card info \u2192';
-        scope.linksTo = '#/creditCard';
+        scope.text = 'It looks like we’re missing your payment information. You’ll need to submit that information before you can receive payouts from Castle.';
+        scope.nextStep = 'Submit your payment information \u2192';
+        scope.linksTo = '#/credit-card';
         Auth.getCurrentUser().profile.$loaded().then(function (profileData) {
           if (!profileData.creditCardToken && !profileData.stripeCustomerId) { elem.find('.hidden').removeClass('hidden'); }
           else { elem.find('.error').addClass('hidden'); }
