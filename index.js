f1963e9949406ce0e2c336532484906be96f8c5f
Scott Lowe <scott@entercastle.com>
2015-04-14 16:11:36 -0400

    Updating node to serve on port 80

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.get('/*', function(req, res){
   res.sendFile(__dirname + '/dist/index.html');
 });
 
-var port = 8000;
+var port = 80;
 server.listen(port, function() {
   console.log('server listening on port ' + port);
 });
