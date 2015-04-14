d7770a25189a8ec496f049d9e422a459312d076d
Scott Lowe <scott@entercastle.com>
2015-04-14 15:27:20 -0400

    Purging all routes but application and adding aws deploy script

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,94 +27,21 @@ angular
     'ngTable',
     'internationalPhoneNumber'
   ])
-  .config(function ($routeProvider, ngFabFormProvider) {
+  .config(function ($routeProvider, $locationProvider, ngFabFormProvider) {
     ngFabFormProvider.extendConfig({
       validationsTemplate : 'views/partials/validationMessages.html'
 
     });
+    $locationProvider.html5Mode(true);
     $routeProvider
-      .when('/signup', {
-        templateUrl: 'views/signup.html',
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-      })
-      .when('/logout', {
-        template: '',
-        resolve: {
-          loggedIn: function (Auth) { Auth.logoutCurrentUser(); }
-        }
-      })
-      .when('/creditCard', {
-        redirectTo: '/credit-card'
-      })
-      .when('/credit-card', {
-        templateUrl: 'views/creditCard.html',
-        resolve: { 'requiresAuth': function (Auth) { return Auth.require(); } }
-      })
-      .when('/bankAccount', {
-        redirectTo: '/bank-account'
-      })
-      .when('/bank-account', {
-        templateUrl: 'views/bankAccount.html',
-        resolve: { 'requiresAuth': function (Auth) { return Auth.require(); } }
-      })
-      .when('/welcome', {
-        templateUrl: 'views/welcome.html',
-      })
       .when('/apply/:address', {
         templateUrl: 'views/prescreen.html',
       })
       .when('/apply', {
         templateUrl: 'views/prescreen.html',
       })
-      .when('/inherited/:address', {
-        templateUrl: 'views/inherited.html',
-      })
-      .when('/inherited/', {
-        templateUrl: 'views/inherited.html',
-      })
-      .when('/properties', {
-        controller: 'PropertiesController as vm',
-        templateUrl: 'views/properties.html',
-        resolve: { 'requiresAuth': function (Auth) { return Auth.require(); } }
-      })
-      .when('/properties/add', {
-        templateUrl: 'views/addProperty.html',
-        resolve: { 'requiresAuth': function (Auth) { return Auth.require(); } }
-      })
-      .when('/properties/:propertyId', {
-        templateUrl: 'views/property.html',
-        resolve: { 'requiresAuth': function (Auth) { return Auth.require(); } }
-      })
-      .when('/tenants/dashboard', {
-        controller: 'TenantDashboardController as vm',
-        templateUrl: 'views/tenant/dashboard.html',
-        resolve: {
-          'requiresAuth': function (Auth) { return Auth.require(); },
-          'requireBankAccount': function (Tenant) { return Tenant.requireBankAccount(); }
-        }
-      })
-      .when('/tenants/contact', {
-        templateUrl: 'views/tenant/contact.html',
-      })
-      .when('/tenants/:tenantId/signup', {
-        controller: 'TenantSignupController as vm',
-        templateUrl: 'views/tenant/signup.html',
-      })
-      .when('/tenants/:tenantId', {
-        templateUrl: 'views/tenant.html',
-      })
-      .when('/password-reset/:email/:token', {
-        controller: 'PasswordResetController as vm',
-        templateUrl: 'views/passwordreset.html',
-      })
-      .when('/password-reset/send', {
-        controller: 'PasswordResetEmailController as vm',
-        templateUrl: 'views/passwordresetemail.html'
-      })
       .otherwise({
-        redirectTo: '/properties'
+        redirectTo: '/apply'
       });
   })
   .run(function ($window, $rootScope, $location, STRIPE_PUBLISHABLE_KEY) {
---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var server = express();
+server.use('/fonts', express.static(__dirname + '/dist/fonts'));
+server.use('/images', express.static(__dirname + '/dist/images'));
+server.use('/scripts', express.static(__dirname + '/dist/scripts'));
+server.use('/styles', express.static(__dirname + '/dist/styles'));
+server.use('/views', express.static(__dirname + '/dist/views'));
+
+server.get('/*', function(req, res){
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+var port = 8000;
+server.listen(port, function() {
+  console.log('server listening on port ' + port);
+});
